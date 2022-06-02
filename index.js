e6fe92377556a6fa26ba7d96ba45842de814708b
Earnest Achayo <earnest.achayo@student.moringaschool.com>
2022-06-02 17:04:04 +0300

    commit tree c5155701a33213434022aa5fb777044d28126c5f

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ introductionWithLanguage("Aki", "Ember.js");
 introductionWithLanguage("Samip", "React");
 
 
-function introductionWithLanguage(name, language="Javascript"){
+function introductionWithLanguageOptional(name, language = "JavaScript") {
     return `Hi, my name is ${name} and I am learning to program in ${language}.`;
 }
-introductionWithLanguage("Gracie");
+introductionWithLanguageOptional("Gracie");
 
 function introductionWithLanguage(name, language){
     return `Hi, my name is ${name} and I am learning to program in ${language}.`;
