5108a3f3330eab48b4feff5f5172596fe1a83f8c
Earnest Achayo <earnest.achayo@student.moringaschool.com>
2022-06-02 15:12:33 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,11 @@
+function introduction(name) {
+    rconsole.log(`Hi, my name is ${name}!`);
+}
+introduction("Aki");
+introduction("Aki");
+
+function introductionWithLanguage(name, language){
+    console.log(`Hi, my name is ${name} and I am learning to program in ${language}.`);
+}
+introductionWithLanguage("Aki", "Ember.js") 
+introductionWithLanguage("Samip", "React") 
